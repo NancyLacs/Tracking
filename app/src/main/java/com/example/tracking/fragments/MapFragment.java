cef201ce21c453996926565f3e7d921c23ff23be
Nancy Agapito Lacsamana <nla031@post.uit.no>
2021-11-07 23:10:41 +0100

    Map working

--- app/src/main/java/com/example/tracking/fragments/MapFragment.java
+++ app/src/main/java/com/example/tracking/fragments/MapFragment.java
@@ -1,17 +1,70 @@
 package com.example.tracking.fragments;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.tracking.BuildConfig;
 import com.example.tracking.R;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.advancedpolyline.MonochromaticPaintList;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
 
 public class MapFragment extends Fragment {
 
+    private View view;
+    private static final int REQUEST_CHECK_SETTINGS = 10;
+    private static final int CALLBACK_ALL_PERMISSIONS = 1;
+    //permissions til lokasjon
+    private static String[] requiredLocationPermissions = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.FOREGROUND_SERVICE
+    };
+
+    private MapView map_view;
+    // Til kart
+    private Marker startMarker;
+    private Marker waypointMarker;
+    private Marker endMarker;
+    private Marker currentPositionMarker; //for person
+    private CompassOverlay mCompassOverlay;
+    private RotationGestureOverlay mRotationGestureOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private Polyline mPolyline;
+    private Polyline plannedRoute;
+    private boolean autoCentering = true;
+    private boolean tracking = false;
 
     public MapFragment() {
         // Required empty public constructor
@@ -29,4 +82,135 @@ public class MapFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        this.view = view;
+        verifyPermissions();
+    }
+
+    private void initMap(View view) {
+        final Context ctx = requireActivity().getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        map_view = view.findViewById(R.id.map_view);
+        startMarker = new Marker(map_view);//for start marker
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+        startMarker.setTitle("Startpunkt");
+        startMarker.setIcon(getResources().getDrawable(R.drawable.ic_start_sign, null));
+
+
+        waypointMarker = new Marker(map_view);
+        waypointMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+        waypointMarker.setTitle("Mellompunkt");
+        waypointMarker.setIcon(getResources().getDrawable(R.drawable.ic_waypoint, null));
+
+        endMarker = new Marker(map_view);
+        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+        endMarker.setTitle("Sluttpunkt");
+        endMarker.setIcon(getResources().getDrawable(R.drawable.ic_flag, null));
+
+        currentPositionMarker = new Marker(map_view);
+        currentPositionMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+        currentPositionMarker.setTitle("Min posisjon");
+        currentPositionMarker.setIcon(getResources().getDrawable(R.drawable.ic_my_location, null));
+
+        this.mPolyline = new Polyline(map_view);
+
+        final Paint paintInside = new Paint();
+        paintInside.setStrokeWidth(7);
+        paintInside.setStyle(Paint.Style.FILL);
+        paintInside.setColor(Color.RED);
+        paintInside.setStrokeCap(Paint.Cap.ROUND);
+        paintInside.setAntiAlias(true);
+        mPolyline.getOutlinePaintLists().add(new MonochromaticPaintList(paintInside));
+        map_view.getOverlays().add(mPolyline);
+
+        //map_view.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        map_view.setTileSource(TileSourceFactory.MAPNIK);
+        map_view.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT);
+        map_view.setMultiTouchControls(true);
+        map_view.getController().setZoom(14.0);
+
+
+        // Compass overlay;
+        this.mCompassOverlay = new CompassOverlay(getContext(), new InternalCompassOrientationProvider(getContext()), map_view);
+        this.mCompassOverlay.enableCompass();
+        map_view.getOverlays().add(this.mCompassOverlay);
+
+        // Multi touch:
+        mRotationGestureOverlay = new RotationGestureOverlay(getContext(), map_view);
+        mRotationGestureOverlay.setEnabled(true);
+        map_view.setMultiTouchControls(true);
+        map_view.getOverlays().add(this.mRotationGestureOverlay);
+
+        // Zoom-knapper;
+        final DisplayMetrics dm = getResources().getDisplayMetrics();
+        mScaleBarOverlay = new ScaleBarOverlay(map_view);
+        mScaleBarOverlay.setCentred(true);
+        //play around with these values to get the location on screen in the right place for your application
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        map_view.getOverlays().add(this.mScaleBarOverlay);
+        //map_view.setVerticalMapRepetitionEnabled(false);
+
+        plannedRoute = new Polyline(map_view);
+        final Paint paint = new Paint();
+        paint.setStrokeWidth(5);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLUE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setAntiAlias(true);
+        plannedRoute.getOutlinePaintLists().add(new MonochromaticPaintList(paint));
+        map_view.getOverlays().add(plannedRoute);
+
+        /*if (tripId > 0) { //når man skal gå inn på kart tripId
+            chooseActionByStatus();
+        }*/
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            //Kalles når bruker har akseptert og gitt tillatelse til bruk av posisjon:
+            case REQUEST_CHECK_SETTINGS:
+                //initLocationUpdates();
+                return;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case CALLBACK_ALL_PERMISSIONS:
+                if (grantResults.length > 0 && grantResults[0] == android.content.pm.PackageManager.PERMISSION_GRANTED) {
+                    this.initMap(view);
+                }
+                return;
+            default:
+                Toast.makeText(requireActivity(), "Feil ...! Ingen tilgang!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean hasPermissions(String... permissions) {
+        if (permissions != null) {
+            for (String permission : permissions) {
+                if (ContextCompat.checkSelfPermission(requireActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void verifyPermissions() {
+        // Kontrollerer om vi har tilgang til eksternt område:
+        if (!hasPermissions(requiredLocationPermissions)) {
+            requestPermissions(requiredLocationPermissions, CALLBACK_ALL_PERMISSIONS);
+        } else {
+            initMap(view);
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/tracking/fragments/NewTripFragment.java
+++ app/src/main/java/com/example/tracking/fragments/NewTripFragment.java
@@ -2,17 +2,24 @@ package com.example.tracking.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.tracking.R;
 
 
 public class NewTripFragment extends Fragment {
 
+    private Button btNewTrip;
     public NewTripFragment() {
         // Required empty public constructor
     }
@@ -29,5 +36,19 @@ public class NewTripFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_trip, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        btNewTrip = view.findViewById(R.id.btSaveNewTrip);
+        NavController navController = Navigation.findNavController(view);
+
+        btNewTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavDirections action = NewTripFragmentDirections.actionNewTripFragmentToMapFragment();
+                navController.navigate(action);
+            }
+        });
     }
 }
